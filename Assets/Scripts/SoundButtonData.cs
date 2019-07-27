30d0fd42b7533e60a6809cf4f6915c67157386fe
AlexBourgeois <abourgeois27380@gmail.com>
2019-07-27 13:38:02 +0200

    Initial commit, all textures

---
+++ Assets/Scripts/SoundButtonData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New SoundButton", menuName = "SoundButton")]
+public class SoundButtonData : ScriptableObject
+{
+    public Sprite artwork;
+    public AudioClip sound;
+}
---
+++ Assets/Scripts/SoundButtonDisplay.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class SoundButtonDisplay : MonoBehaviour
+{
+    public Image artwork;
+    public AudioSource audio;
+
+    public SoundButtonData data;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        artwork.sprite = data.artwork;
+        audio.clip = data.sound;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
